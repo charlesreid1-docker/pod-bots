39e1ad7412236bdbb142d6ddcd227746941c7388
Charles Reid <charlesreid1@gmail.com>
2018-05-14 02:54:07 -0700

    update the update submodules script

--- update_submodules.sh
+++ update_submodules.sh
@@ -2,13 +2,16 @@
 
 GREP="`which grep`"
 
-for dir in `/bin/ls -1 | $GREP "d-"`; do
+for dir in `find . -type d -maxdepth 1 | $GREP -v "docs" | $GREP -v ".git"`; do
+    echo "--------------------"
+    echo $dir
+    (
     cd $dir
     git checkout master
     git pull origin master
-    cd ..
+    )
 done
 
-git commit -a -m 'Updating submodules to latest'
-git push origin master
+#git commit -a -m 'Updating submodules to latest'
+#git push origin master
 
